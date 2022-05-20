87beb3b839040e3192b17a2121d412e9e1cef997
Renato Fekete <renatofekete.91@gmail.com>
2022-05-20 16:57:56 +0200

    home page

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 import Page from '../../components/Page/Page'
+import styling from './HomePage.module.css'
 
 function HomePage({message}: {message: string}): JSX.Element {
   useEffect(() => {
@@ -8,10 +9,16 @@ function HomePage({message}: {message: string}): JSX.Element {
 
   return(
     <Page message={message}>
-      <article>
-        <h1>Home page</h1>
-        <a href='https://github.com/renatofekete/q-posts' target='_blank' rel="noreferrer noopener">Github repository</a>
-        <p>Approximate time needed to finish the assignment: 12h</p>
+      <article className={styling.page}>
+        <h1 className={styling.title}>Home page</h1>
+        <p><a href='https://github.com/renatofekete/q-posts' target='_blank' rel="noreferrer noopener">Github repository</a></p>
+        <p>Approximate time needed to finish the assignment: <b>12h</b></p>
+        <p>Build/run process description:</p>
+        <ul>
+          <li>git clone https://github.com/renatofekete/q-posts.git or download ZIP to a folder</li>
+          <li>run <code>yarn</code> in downloaded folder to install dependencies</li>
+          <li>run <code>yarn start</code> to run app in development mode on <a href="http://localhost:3000">http://localhost:3000</a></li>
+        </ul>
       </article>
     </Page>
   )
